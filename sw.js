5406548a3d3a31b8ff1a0dc50d8b4e65ea56b9ef
Nunah31 <estherhabari@gmail.com>
2026-05-27 19:09:32 +0300

    הוספת PWA ו-Service Worker

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE = 'shows-v1';
+const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(
+  fetch(e.request).catch(() => caches.match(e.request))
+));
