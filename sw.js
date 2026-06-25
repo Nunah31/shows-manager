3cdb015823de1b30df939a963c186c5bf33bc1dd
Nunah31 <nunah31@users.noreply.github.com>
2026-06-25 20:13:18 +0300

    Force service worker to update immediately and clear old caches

--- sw.js
+++ sw.js
@@ -1,6 +1,22 @@
-const CACHE = 'shows-v1';
+const CACHE = 'shows-v2';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', e => e.respondWith(
-  fetch(e.request).catch(() => caches.match(e.request))
+  fetch(e.request).then(res => {
+    caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+    return res;
+  }).catch(() => caches.match(e.request))
 ));
