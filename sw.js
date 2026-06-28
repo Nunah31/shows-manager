909bdccf1877228caf5aced050bdefc0a02da17a
Nunah31 <nunah31@users.noreply.github.com>
2026-06-28 14:44:08 +0300

    Add force-refresh button in settings as manual escape hatch for stale caches

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shows-v6';
+const CACHE = 'shows-v7';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
