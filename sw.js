dbf4911a73246d4a7db162f9fd125289e79327ea
Nunah31 <nunah31@users.noreply.github.com>
2026-06-28 15:03:14 +0300

    Fix finance app not picking up new entries: update savedAt timestamp when writing from shows app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shows-v9';
+const CACHE = 'shows-v10';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
